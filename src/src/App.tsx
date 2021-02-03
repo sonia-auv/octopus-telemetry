2a9996527fb6af8721aa5809e4862fe6c5e17e34
Carol <c_maro1@yahoo.fr>
2021-02-03 16:04:00 -0500

    added ros initialization

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as Roslib from 'roslib'
 
+var ros = new Roslib.Ros({
+    url: 'ws://localhost:9090'
+})
+
+ros.on('connection', function(){
+    console.log('Connected')
+})
+
+ros.on('error', function(error){
+    console.log('Error: ' + error)
+})
+
+ros.on('close', function(){
+    console.log('Connection closed')
+})
+
+var listener = new Roslib.Topic({
+    ros: ros,
+    name: '/proc_navigation/odom',
+    messageType: 'nav_msgs/Odometry'
+})
+
+listener.subscribe(function(message){
+    console.log('Received message on ' + listener.name + ' : ' + JSON.stringify(message))
+    listener.unsubscribe()
+})
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>WIP</p>
     </div>
   );
 }

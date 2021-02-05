3151f44c46d463e19469eafbadb657fb8dc5620d
Carol <c_maro1@yahoo.fr>
2021-02-05 12:27:11 -0500

    proof of concept

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,40 +1,186 @@
-import React from 'react';
-import './App.css';
-import * as Roslib from 'roslib'
+import React, { useState, useEffect, useCallback} from "react";
+import * as ROSLIB from 'roslib';
+import Slider from '@material-ui/core/Slider'
+import GridLayout from 'react-grid-layout'
+import { useROSTopicSubscriber } from "./hooks/useROSTopicSubscriber";
 
-var ros = new Roslib.Ros({
-    url: 'ws://localhost:9090'
-})
+export const App = () => {
 
-ros.on('connection', function(){
-    console.log('Connected')
-})
+    const [readMode, setReadMode] = useState(true);
+    const [thrusters, setThrusters] = useState<{ ID: number, effort: number }[]>(
+        [
+            {ID: 1, effort: 0},
+            {ID: 2, effort: 0},
+            {ID: 3, effort: 0},
+            {ID: 4, effort: 0},
+            {ID: 5, effort: 0},
 
-ros.on('error', function(error){
-    console.log('Error: ' + error)
-})
 
-ros.on('close', function(){
-    console.log('Connection closed')
-})
+        ]);
 
-var listener = new Roslib.Topic({
-    ros: ros,
-    name: '/proc_navigation/odom',
-    messageType: 'nav_msgs/Odometry'
-})
+    const thrusterEffortCallback = useCallback(
+        (x: any) => {
+            let data = x.data
+            let parsed = JSON.parse(data)
+            setThrusters([
+                {ID: 1, effort:parsed[1]},
+                {ID: 2, effort:parsed[2]},
+                {ID: 3, effort:parsed[3]},
+                {ID: 4, effort:parsed[4]},
+                {ID: 5, effort:parsed[5]},
 
-listener.subscribe(function(message){
-    console.log('Received message on ' + listener.name + ' : ' + JSON.stringify(message))
-    listener.unsubscribe()
-})
-function App() {
+                ])
+        },
+        []
+    )
 
-  return (
-    <div className="App">
-      <p>WIP</p>
-    </div>
-  );
+    useROSTopicSubscriber<any>(thrusterEffortCallback, "/testPublisher", "std_msgs/String") // TODO Set message type
+
+
+
+    const style = { height: 'calc(100% - 55px)' };
+    console.log(thrusters)
+    const marks = [
+        {
+            value: -100,
+            label: 100
+        },
+        {
+            value: -75,
+            label: -75
+        },
+        {
+            value: -50,
+            label: -50
+        },
+        {
+            value: -25,
+            label: -25
+        },
+        {
+            value: 0,
+            label: 0
+        },
+        {
+            value: 25,
+            label: 25
+        },
+        {
+            value: 50,
+            label: 50
+        },
+        {
+            value: 75,
+            label: 75
+        },
+        {
+            value: 100,
+            label: 100
+        }]
+    return (
+        <div className="margin-top" style={style} >
+            <GridLayout className="layout"
+                        cols={12}
+                        rowHeight={200}
+                        width={1200}
+                        verticalCompact={false}>
+                <div key="a"
+                     data-grid={{x: 4, y: 0, w: 4, h: 4}}
+                     style={{height: 400}}>
+                    <Slider
+                        orientation= "vertical"
+                        value={thrusters[0].effort}
+                        min={-100}
+                        max={100}
+                        valueLabelDisplay="on"
+                        marks={marks}
+                    />
+                    <Slider
+                        orientation= "vertical"
+                        value={thrusters[1].effort}
+                        min={-100}
+                        max={100}
+                        valueLabelDisplay="on"
+                        marks={marks}
+                    />
+                    <Slider
+                        orientation= "vertical"
+                        value={thrusters[2].effort}
+                        min={-100}
+                        max={100}
+                        valueLabelDisplay="on"
+                        marks={marks}
+                    />
+                    <Slider
+                        orientation= "vertical"
+                        value={thrusters[3].effort}
+                        min={-100}
+                        max={100}
+                        valueLabelDisplay="on"
+                        marks={marks}
+                    />
+                    <Slider
+                        orientation= "vertical"
+                        value={thrusters[4].effort}
+                        min={-100}
+                        max={100}
+                        valueLabelDisplay="on"
+                        marks={marks}
+                    />
+                </div>
+
+
+                <div key="b"
+                     data-grid={{x: 4, y: 0, w: 4, h: 4}}
+                     style={{height: 400}}>
+                    <Slider
+                        orientation= "vertical"
+                        value={thrusters[0].effort}
+                        min={-100}
+                        max={100}
+                        valueLabelDisplay="on"
+                        marks={marks}
+                    />
+                    <Slider
+                        orientation= "vertical"
+                        value={thrusters[1].effort}
+                        min={-100}
+                        max={100}
+                        valueLabelDisplay="on"
+                        marks={marks}
+                    />
+                    <Slider
+                        orientation= "vertical"
+                        value={thrusters[2].effort}
+                        min={-100}
+                        max={100}
+                        valueLabelDisplay="on"
+                        marks={marks}
+                    />
+                    <Slider
+                        orientation= "vertical"
+                        value={thrusters[3].effort}
+                        min={-100}
+                        max={100}
+                        valueLabelDisplay="on"
+                        marks={marks}
+                    />
+                    <Slider
+                        orientation= "vertical"
+                        value={thrusters[4].effort}
+                        min={-100}
+                        max={100}
+                        valueLabelDisplay="on"
+                        marks={marks}
+                    />
+                </div>
+
+            </GridLayout>
+
+
+        </div>
+    );
+
 }
 
 export default App;

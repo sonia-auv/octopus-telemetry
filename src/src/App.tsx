79cac98ac8bb510dc46c0de9e9c79c1cd311f1a2
antho622 <38926818+antho622@users.noreply.github.com>
2021-02-11 15:14:44 -0500

    Add styling for thrusters

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -4,19 +4,16 @@ import { useROSTopicSubscriber } from "./hooks/useROSTopicSubscriber";
 import {ThrustersForm} from './components/ThrustersForm'
 import {Thruster} from './components/Thruster'
 
-
 export const App = () => {
 
     const [readMode, setReadMode] = useState(true);
-    const [thrusters, setThrusters] = useState<{ ID: number, effort: number }[]>(
+    const [thrusters, setThrusters] = useState<{ ID: number, effort: number, thumbEnabled: boolean }[]>(
         [
-            {ID: 1, effort: 0},
-            {ID: 2, effort: 0},
-            {ID: 3, effort: 0},
-            {ID: 4, effort: 0},
-            {ID: 5, effort: 0},
-
-
+            {ID: 1, effort: 0, thumbEnabled: true},
+            {ID: 2, effort: 0, thumbEnabled: false},
+            {ID: 3, effort: 0, thumbEnabled: true},
+            {ID: 4, effort: 0, thumbEnabled: false},
+            {ID: 5, effort: 0, thumbEnabled: true},
         ]);
 
     const thrusterEffortCallback = useCallback(
@@ -24,12 +21,11 @@ export const App = () => {
             let data = x.data
             let parsed = JSON.parse(data)
             setThrusters([
-                {ID: 1, effort:parsed[1]},
-                {ID: 2, effort:parsed[2]},
-                {ID: 3, effort:parsed[3]},
-                {ID: 4, effort:parsed[4]},
-                {ID: 5, effort:parsed[5]},
-
+                {ID: 1, effort:parsed[1], thumbEnabled: true},
+                {ID: 2, effort:parsed[2], thumbEnabled: false},
+                {ID: 3, effort:parsed[3], thumbEnabled: true},
+                {ID: 4, effort:parsed[4], thumbEnabled: false},
+                {ID: 5, effort:parsed[5], thumbEnabled: true},
                 ])
         },
         []
@@ -42,12 +38,13 @@ export const App = () => {
         <div className="margin-top" style={style} >
             <GridLayout className="layout"
                         cols={12}
-                        rowHeight={200}
+                        rowHeight={100}
                         width={1200}
-                        verticalCompact={false}>
+                        verticalCompact={false}
+                        draggableCancel={".MuiSlider-valueLabel, .MuiSlider-thumb"}>
                 <div key="a"
-                     data-grid={{x: 4, y: 0, w: 4, h: 4}}
-                     style={{height: 400}}>
+                     data-grid={{x: 2, y: 0, w: 8, h: 5 , minW:8, maxW:12, minH:3, maxH:6}}
+                     style={{display: 'flex'}}>
                     {thrusters.map((thruster, id) =>{
                         return (
                             <Thruster key={id}
@@ -55,21 +52,20 @@ export const App = () => {
                                       identification={thruster.ID}
                                       minMark = {-100}
                                       maxMark={100}
-                                      step={15}/>
+                                      step={25}
+                                      thumbEnabled={thruster.thumbEnabled}
+                                      />
                         )
                     })}
                 </div>
                 <div key="b"
-                    data-grid={{x: 0, y: 0, w: 1, h: 1}}
+                    data-grid={{x: 0, y: 0, w: 2, h: 2}}
                     style={{height: 600}}>
                     <ThrustersForm />
                 </div>
             </GridLayout>
-
-
         </div>
     );
-
 }
 
 export default App;

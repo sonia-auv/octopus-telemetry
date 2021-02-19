445531d86ef5fb3b024782672609e5b36ea4215b
antho622 <38926818+antho622@users.noreply.github.com>
2021-02-19 17:37:52 -0500

    Add actuators
    Ajout des actuators

---
+++ src/src/components/ActuatorModule.tsx
@@ -0,0 +1,83 @@
+import React, { useCallback, useContext } from 'react';
+import Switch from './Switch';
+import { GeneralContext } from "../context/generalContext";
+import { Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import { useROSService } from '../hooks/useROSService'
+import ROSLIB from "roslib";
+
+const ActuatorModule = () => {
+
+    const c = useContext(GeneralContext);
+
+    const ButtonStyle = withStyles({
+        contained: {
+            backgroundColor: 'lightgrey',
+            border: '2px solid rgba(0, 0, 0, 1.0)'
+        },
+
+    })(Button);
+
+    // Reponse en retour a l appel du service
+    const actuactorServiceCallback = useCallback(
+        (x: any) => {
+            console.log(x)
+        }, []
+    )
+
+    const actuactorServiceCall = useROSService<any>(actuactorServiceCallback, "/provider_actuators/do_action_srv", "provider_actuators")
+
+    // FORMATAGE DU MESSAGE A ENVOYER AU SERVICE A VERIFIER
+    const HandleChangeSwitch = (value: any) => {
+
+        console.log('value:', value)
+
+        var request = new ROSLIB.ServiceRequest({
+            ELEMENT_ARM: 2,
+            ARM_OPEN: !value,
+            ACTION_ARM_EXEC: 1
+        });
+        actuactorServiceCall(request)
+    } 
+
+    // FORMATAGE DU MESSAGE A ENVOYER AU SERVICE A VERIFIER
+    const handleChangeButtonTorpedo = () => {
+        var request = new ROSLIB.ServiceRequest({
+            ELEMENT_TORPEDO: 0,
+            SIDE_PORT: 0,
+            ACTION_DROPPER_LAUCH: 1
+        });
+        actuactorServiceCall(request)
+    }
+
+    // FORMATAGE DU MESSAGE A ENVOYER AU SERVICE A VERIFIER
+    const handleChangeButtonDropObject = () => {
+        var request = new ROSLIB.ServiceRequest({
+            ELEMENT_TORPEDO: 1,
+            SIDE_PORT: 0,
+            ACTION_DROPPER_LAUCH: 1
+        });
+        actuactorServiceCall(request)
+    }
+
+    return (
+        <GeneralContext.Consumer>
+            {context => context && (
+                <div style={{ width: '100%', height: '100%', flexDirection: 'row', backgroundColor: '#85D2BB', textAlign: 'center' }}>
+                    <h1 style={{ fontSize: '20px', textAlign: 'center' }}>ROBOTIC ARM</h1>
+                    <Switch onLabel="Open"
+                        offLabel="Closed"
+                        vertical={false}
+                        value={!context.isRoboticArmClosed}
+                        handler={() => {context.setIsRoboticArmClosed(!context.isRoboticArmClosed); HandleChangeSwitch(!context.isRoboticArmClosed)}} />
+                    <h1 style={{ fontSize: '20px', textAlign: 'center' }}>TORPEDO</h1>
+                    <ButtonStyle variant='contained' style={{ fontSize: '20px', alignSelf: 'center' }} onClick={handleChangeButtonTorpedo}>LAUNCH</ButtonStyle>
+                    <h1 style={{ fontSize: '20px', textAlign: 'center' }}>DROP OBJECT</h1>
+                    <ButtonStyle variant='contained' style={{ fontSize: '20px', alignSelf: 'center' }} onClick={handleChangeButtonDropObject}>DROP</ButtonStyle>
+                </div>
+            )}
+        </GeneralContext.Consumer>
+    );
+};
+
+export default ActuatorModule;
---
+++ src/src/hooks/useROSService.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect, useContext } from "react";
+import ROSLIB from "roslib";
+import { RosContext } from "../context/rosContext";
+
+export const useROSService = <F>(
+    callback: (m: F) => void,
+    name: string,
+    serviceType: string
+  ) => {
+    const ros = useContext(RosContext);
+    const [service, setService] = useState<ROSLIB.Service| null>(null);
+  
+    useEffect(() => {
+      setService(
+        new ROSLIB.Service({
+          ros,
+          name,
+          serviceType,
+        })
+      );
+    }, [ros]);
+  
+    return (x: ROSLIB.ServiceRequest) => {
+      if (service) {
+        service.callService(x, (y) => callback(y as F));
+      } 
+    } 
+  };

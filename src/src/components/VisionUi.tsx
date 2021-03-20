003aa6d3a839a3cb4afc29fd9a26e48be24536e7
antho622 <38926818+antho622@users.noreply.github.com>
2021-03-19 20:17:36 -0400

    Two tab completed

--- src/src/components/VisionUi.tsx
+++ src/src/components/VisionUi.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { GeneralContext } from "../context/generalContext";
 import AppBar from '@material-ui/core/AppBar';
@@ -7,7 +7,6 @@ import Tab from '@material-ui/core/Tab';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
-import ImageViewer from "../components/ImageViewer";
 import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select';
 import FormControl from '@material-ui/core/FormControl';
@@ -25,6 +24,9 @@ import SaveIcon from '@material-ui/icons/Save';
 import RestoreIcon from '@material-ui/icons/Restore';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { useROSService } from '../hooks/useROSService'
+import ROSLIB from "roslib";
+import CachedIcon from '@material-ui/icons/Cached';
 
 TabPanel.propTypes = {
     children: PropTypes.node,
@@ -105,11 +107,11 @@ const VisionUIModule = () => {
     ////////////////////////////////
 
     const [name, setName] = useState('');
-    const [filterChainSelected, setFilterChainSelected] = useState(null);
-    const [filterChainList, setfilterChainList] = useState(["filter1", "filter2"]);
-    const [mediaSelected, setMediaSelected] = useState(null);
-    const [mediaList, setMediaList] = useState(["media1", "media2"]);
-    const [file, setFile] = useState('No file');
+    const [filterChainSelected, setFilterChainSelected] = useState('');
+    const [filterChainList, setfilterChainList] = useState<[]>([]);
+    const [mediaSelected, setMediaSelected] = useState('');
+    const [mediaList, setMediaList] = useState<[]>([]);
+    const [file, setFile] = useState('');
     const [topicName, setTopicName] = useState('');
 
     const handleNameChange = (e: any) => {
@@ -128,6 +130,7 @@ const VisionUIModule = () => {
     }
 
     const handleChangeFilterChain = (x: any) => {
+        
         setFilterChainSelected(x.target.value)
     }
 
@@ -135,20 +138,124 @@ const VisionUIModule = () => {
         setMediaSelected(x.target.value)
     }
 
+    function fileDialogClicked(event: any)  {
+        setFile(event.target.files[0].name)
+    }
+
+    const inputFile = useRef<HTMLInputElement>(null) 
+
     const handleFileOpen = (value: any) => {
-        setFile('my file path....')
+
+        if (inputFile.current) {
+            inputFile.current.click();
+        }
+
     }
 
     const handleTopicNameChange = (e: any) => {
         setTopicName(e.target.value)
     }
 
+    const startMediaCmdServiceCallback = useCallback(
+        (x: any) => {
+        }, []
+    )
+
+    const executeCmdServiceCallback = useCallback(
+        (x: any) => {
+        }, []
+    )
+
+    // Start media service
+    const startMediaCmdServiceCall = useROSService<any>(startMediaCmdServiceCallback, "/provider_vision/start_stop_camera", "sonia_common/StartStopMedia")
+    // Execute command service
+    const executeCmdServiceCall = useROSService<any>(executeCmdServiceCallback, "/proc_image_processing/execute_cmd", "sonia_common/ExecuteCmd")
+
     const handleCreate = (value: any) => {
 
+        if (name !== '') 
+        {
+            //Topic mode
+            var media = topicName
+  
+            // Video mode
+            if (media === '' && file !== '')
+            {
+                media = file
+                // TODO: check formatting of the service request
+                var request = new ROSLIB.ServiceRequest({ camera_name: media, start:1 });
+                startMediaCmdServiceCall(request)
+                media = '/provider_vision' + media.replace('.','')
+
+            }
+            // Normal mode
+            if (media === '') 
+            {
+                media = mediaSelected
+            }
+
+            if (media !== '' && filterChainSelected !== '') {
+                var request = new ROSLIB.ServiceRequest({ node_name: name, filterchain_name: filterChainSelected, media_name: media, start: 1 });
+                executeCmdServiceCall(request)
+                console.log(name, filterChainSelected, media)
+            }
+        }
     }
 
     const handleFilterChainNameChange = (e: any) => {
         setFilterChainName(e.target.value)
+    }
+
+    const getFilterChainlistServiceCallback = useCallback(
+        (x: any) => {
+
+            // TODO: not tested with ros, , we are supposed to receive a string with ; separator inside x
+            var receivedList = "simple_body_baby;deep_front;simple_test;simple_bin_handle"
+
+            var tab: any = []
+            receivedList.split(';').forEach((v: String, index: number) => {
+                tab.push( { value: v } )
+            });
+
+            // Sort values
+            tab.sort((a:any, b:any) => a.value !== b.value  ? a.value  < b.value  ? -1 : 1 : 0);
+
+            setfilterChainList(tab);
+
+        }, []
+    )
+
+    const getFilterChainlistServiceCall = useROSService<any>(getFilterChainlistServiceCallback, "/proc_image_processing/get_information_list", "sonia_common/GetInformationList")
+
+    const handleRefreshFilterChainList = () => {
+        var request = new ROSLIB.ServiceRequest(3);
+        getFilterChainlistServiceCall(request)
+    }
+
+    const getMedialistServiceCallback = useCallback(
+        (x: any) => {
+
+            // TODO: not tested with ros, we are supposed to receive a string with ; separator inside x
+            var receivedList = "/proc_image_processing/sonar_map;/provider_vision/Front_GigE"
+
+            var tab: any = []
+            receivedList.split(';').forEach((v: String, index: number) => {
+                tab.push( { value: v } )
+            });
+
+            // Sort values
+            tab.sort((a:any, b:any) => a.value !== b.value  ? a.value  < b.value  ? -1 : 1 : 0);
+
+            setMediaList(tab);
+
+        }, []
+    )
+
+    const getMedialistServiceCall = useROSService<any>(getMedialistServiceCallback, "/proc_image_processing/get_information_list", "sonia_common/GetInformationList")
+
+    const handleRefreshMediaList = () => {
+        var request = new ROSLIB.ServiceRequest(2);
+        getMedialistServiceCall(request)
     }
 
     ////////////////////////////////
@@ -165,29 +272,83 @@ const VisionUIModule = () => {
     })(List);
 
     const [filterChainName, setFilterChainName] = useState('');
-    const [filterChainListItems, setFilterChainListItems] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    const [filterChainSelectedTab, setFilterChainSelectedTab] = React.useState('');
+
+    const addFilterChainServiceCallback = useCallback(
+        (x: any) => {
+        }, []
+    )
+
+    const addFilterChainServiceCall = useROSService<any>(addFilterChainServiceCallback, "/proc_image_processing/manage_filterchain", "sonia_common/ManageFilterchain");
 
     const handleAddFilterChain = (value: any) => {
 
+        if (filterChainName != '') {
+            // TODO: formatting of ros message to check
+            var request = new ROSLIB.ServiceRequest({ filterchain: filterChainName, add: 1 });
+            addFilterChainServiceCall(request)
+
+            //Refresh filter chain list
+            var request2 = new ROSLIB.ServiceRequest(3);
+            getFilterChainlistServiceCall(request2)
+        }
     }
+
+    const handSelectedFilterChain = (event: any, value: any) => {
+        setFilterChainSelectedTab(value);
+    };
+
+    const copyFilterChainServiceCallback = useCallback(
+        (x: any) => {
+        }, []
+    )
+
+    const copyFilterChainServiceCall = useROSService<any>(copyFilterChainServiceCallback, "/proc_image_processing/copy_filterchain", "sonia_common/CopyFilterchain");
 
     const handleCloneFilterChain = (value: any) => {
 
+        if (filterChainSelectedTab !== '' && filterChainName !== '') {
+
+            // TODO: formatting of ros message to check
+            var request = new ROSLIB.ServiceRequest({ filterchain_to_copy: filterChainSelectedTab, filterchain_new_name: 'filterchain/' + filterChainName });
+            copyFilterChainServiceCall(request)
+
+            //Refresh filter chain list
+            var request2 = new ROSLIB.ServiceRequest(3);
+            getFilterChainlistServiceCall(request2)
+        }
     }
+
+    const deleteFilterChainServiceCallback = useCallback(
+        (x: any) => {
+        }, []
+    )
+
+    const deleteFilterChainServiceCall = useROSService<any>(deleteFilterChainServiceCallback, "/proc_image_processing/manage_filterchain", "sonia_common/ManageFilterchain");
 
     const handleDeleteFilterChain = (value: any) => {
 
+        if (filterChainSelectedTab !== '') {
+
+            // TODO: formatting of ros message to check
+            var request = new ROSLIB.ServiceRequest({ filterchain: 'filterchain/' + filterChainSelectedTab, delete: 2 });
+            deleteFilterChainServiceCall(request)
+
+            //Refresh filter chain list
+            var request2 = new ROSLIB.ServiceRequest(3);
+            getFilterChainlistServiceCall(request2)
+
+        }
     }
 
-    function FilterChainList(props: any) {
-
+    function FilterChainList(props: any) 
+    {
         const { index, style } = props;
-
         return (
             <div>
-                {filterChainListItems.map((item) => (
-                    <ListItem button style={style} key={item}>
-                        <ListItemText primary={item} />
+                {filterChainList.map((item) => (
+                    <ListItem button style={style} key={item} selected={filterChainSelectedTab === item['value']}>
+                        <ListItemText primary={item['value']} onClick={(event: any) => handSelectedFilterChain(event, item['value'])} />
                     </ListItem>
                 ))}
             </div>
@@ -326,11 +487,19 @@ const VisionUIModule = () => {
                                         label="Filter chain"
                                         value={filterChainSelected ? filterChainSelected : "None"}>
                                         <MenuItem value={"None"}>None</MenuItem>
-                                        {filterChainList.map((value, index) => {
-                                            return <MenuItem value={value}>{value}</MenuItem>
+                                        {filterChainList.map((value) => {
+                                            return <MenuItem value={value["value"]}>{value["value"]}</MenuItem>
                                         })}
                                     </Select>
-                                </FormControl><br></br>
+                                </FormControl>
+                                <Button
+                                    variant="contained"
+                                    color="default"
+                                    className={classes.button}
+                                    startIcon={<CachedIcon />}
+                                    onClick={handleRefreshFilterChainList}
+                                ></Button>
+                                <br></br>
                                 <FormControl variant="filled" className={classes.formControl}>
                                     <InputLabel id="selectMedia-outlined-label">Media</InputLabel>
                                     <Select
@@ -341,16 +510,25 @@ const VisionUIModule = () => {
                                         label="Media"
                                         value={mediaSelected ? mediaSelected : "None"}>
                                         <MenuItem value={"None"}>None</MenuItem>
-                                        {mediaList.map((value, index) => {
-                                            return <MenuItem value={value}>{value}</MenuItem>
+                                        {mediaList.map((value) => {
+                                            return <MenuItem value={value["value"]}>{value["value"]}</MenuItem>
                                         })}
                                     </Select>
-                                </FormControl><br></br>
+                                </FormControl>
+                                <Button
+                                    variant="contained"
+                                    color="default"
+                                    className={classes.button}
+                                    startIcon={<CachedIcon />}
+                                    onClick={handleRefreshMediaList}
+                                ></Button>
+                                <br></br>
                                 <div style={{ width: '75%', float: 'left' }} >
                                     <TextField disabled={true} value={file} id="file_id" label="File" variant="outlined" fullWidth={true} style={{ padding: '10px 10px', marginTop: '10px' }} />
                                 </div>
+                                <input type='file' id='file' ref={inputFile} onChange={fileDialogClicked} style={{display: 'none'}}/>
                                 <div style={{ float: 'right' }}><ButtonStyle variant='contained' style={{ fontSize: '20px', marginTop: '22px' }} onClick={handleFileOpen}>...</ButtonStyle></div>
-                                <TextField value={topicName} onChange={handleTopicNameChange} onKeyDown={handleCmdKeyDown} id="visionUi_topicname_id" label="Name" variant="outlined" fullWidth={true} style={{ padding: '10px 10px', marginTop: '10px' }} />
+                                <TextField value={topicName} onChange={handleTopicNameChange} onKeyDown={handleCmdKeyDown} id="visionUi_topicname_id" label="Topic Name" variant="outlined" fullWidth={true} style={{ padding: '10px 10px', marginTop: '10px' }} />
                                 <ButtonStyle variant='contained' style={{ fontSize: '15px', marginTop: '10px', float: 'right' }} onClick={handleCreate}>create</ButtonStyle>
                             </div>
                         </TabPanel>
@@ -359,7 +537,7 @@ const VisionUIModule = () => {
                             <ButtonStyle variant='contained' style={{ fontSize: '15px', marginTop: '10px', float: 'left', marginLeft: '10px' }} onClick={handleAddFilterChain}>Add</ButtonStyle>
                             <ButtonStyle variant='contained' style={{ fontSize: '15px', marginTop: '10px', float: 'left', marginLeft: '10px' }} onClick={handleCloneFilterChain}>Clone</ButtonStyle>
                             <ButtonStyle variant='contained' style={{ fontSize: '15px', marginTop: '10px', float: 'left', marginLeft: '10px' }} onClick={handleDeleteFilterChain}>Delete</ButtonStyle><br></br>
-                            <ListFilterChainStyle><FilterChainList className={classes.root} /></ListFilterChainStyle>
+                            <ListFilterChainStyle><FilterChainList className={classes.root}/></ListFilterChainStyle>
                         </TabPanel>
                         <TabPanel value={value} index={2}>
                             <FormControl variant="filled" className={classes.formControl}>

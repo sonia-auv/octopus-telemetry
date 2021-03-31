fc374ac7243fa6b9527d8f667f034a1f714f078f
antho622 <38926818+antho622@users.noreply.github.com>
2021-03-31 09:53:12 -0400

    Fix theme

--- src/src/components/common/slider/ThrusterControlSlider.tsx
+++ src/src/components/common/slider/ThrusterControlSlider.tsx
@@ -1,6 +1,7 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useContext } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { Slider as MUISlider } from '@material-ui/core';
+import { GeneralContext } from "../../../context/generalContext";
 
 type ThrusterControlSliderProps = {
   value?: number | number[]
@@ -15,89 +16,92 @@ type ThrusterControlSliderProps = {
   handlerChange: (event: React.ChangeEvent<{}>, value: number | number[]) => void;
 };
 
-const GenericThrusterControlSlider = withStyles({
+const Select: FunctionComponent<ThrusterControlSliderProps> = (props) => {
 
-  root: {
-    "&.MuiSlider-vertical": {
-      width: '80px',
-      padding: '0px 10px',
+  const context = useContext(GeneralContext)
+  const GenericThrusterControlSlider = withStyles({
+
+    root: {
+      "&.MuiSlider-vertical": {
+        width: '80px',
+        padding: '0px 10px',
+      },
+      marginLeft: '-132px',
     },
-    marginLeft: '-132px',
-  },
 
-  thumb: {
-    color: '#3796BF',
-    width: '60px',
-    height: '45px',
-    borderRadius: '4px',
-    "&.Mui-disabled": {
+    thumb: {
+      color: '#3796BF',
       width: '60px',
       height: '45px',
       borderRadius: '4px',
-    },
-  },
-
-  vertical: {
-    "& .MuiSlider-thumb": {
-      marginLeft: '0px',
-      marginBottom: '-20px',
       "&.Mui-disabled": {
-        marginBottom: '-20px',
-        marginLeft: '0px',
-      }
-    }
-  },
-  mark: {
-    color: 'black',
-    backgroundColor: 'black',
-    opacity: 1.0,
-    width: '60px',
-  },
-  marked: {
-    "&.MuiSlider-vertical": {
-      marginBottom: '30px',
-      marginTop: '30px',
+        width: '60px',
+        height: '45px',
+        borderRadius: '4px',
+      },
     },
-  },
-  markActive: {
-    color: 'black',
-    backgroundColor: 'black',
-    opacity: 1.0,
-    width: '60px',
-  },
-  rail: {
-    color: 'black',
-    opacity: 1.0,
-    marginLeft: '30px'
-  },
-  track: {
-    color: 'black',
-    opacity: 1.0,
-    marginLeft: '30px'
-  },
-  markLabel: {
-    //color: context.isDarkMode ? "white" : 'black',
-    color: "white",
-    marginLeft: '68px'
-  },
-})(MUISlider);
 
-const Select: FunctionComponent<ThrusterControlSliderProps> = (props) => (
-  <GenericThrusterControlSlider
-    data-testid="test-ThrusterControlSlider"
-    value={props.value}
-    orientation={props.orientation}
-    min={props.min}
-    max={props.max}
-    disabled={props.disabled}
-    valueLabelDisplay={props.valueLabelDisplay}
-    marks={props.marks}
-    ThumbComponent={props.ThumbComponent}
-    defaultValue={props.defaultValue}
-    onChange={props.handlerChange}
-  >
-  </GenericThrusterControlSlider>
-);
+    vertical: {
+      "& .MuiSlider-thumb": {
+        marginLeft: '0px',
+        marginBottom: '-20px',
+        "&.Mui-disabled": {
+          marginBottom: '-20px',
+          marginLeft: '0px',
+        }
+      }
+    },
+    mark: {
+      color: 'black',
+      backgroundColor: 'black',
+      opacity: 1.0,
+      width: '60px',
+    },
+    marked: {
+      "&.MuiSlider-vertical": {
+        marginBottom: '30px',
+        marginTop: '30px',
+      },
+    },
+    markActive: {
+      color: 'black',
+      backgroundColor: 'black',
+      opacity: 1.0,
+      width: '60px',
+    },
+    rail: {
+      color: 'black',
+      opacity: 1.0,
+      marginLeft: '30px'
+    },
+    track: {
+      color: 'black',
+      opacity: 1.0,
+      marginLeft: '30px'
+    },
+    markLabel: {
+      color: context.isDarkMode ? "white" : 'black',
+      marginLeft: '68px'
+    },
+  })(MUISlider);
+
+  return (
+      <GenericThrusterControlSlider
+        data-testid="test-ThrusterControlSlider"
+        value={props.value}
+        orientation={props.orientation}
+        min={props.min}
+        max={props.max}
+        disabled={props.disabled}
+        valueLabelDisplay={props.valueLabelDisplay}
+        marks={props.marks}
+        ThumbComponent={props.ThumbComponent}
+        defaultValue={props.defaultValue}
+        onChange={props.handlerChange}
+      >
+      </GenericThrusterControlSlider>
+  )
+};
 
 Select.defaultProps = {
   disabled: false,
--- src/src/components/common/slider/ThrusterEffortIndicatorSlider.tsx
+++ src/src/components/common/slider/ThrusterEffortIndicatorSlider.tsx
@@ -1,6 +1,7 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useContext } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { Slider as MUISlider } from '@material-ui/core';
+import { GeneralContext } from "../../../context/generalContext";
 
 type ThrusterEffortIndicatorSliderProps = {
     value?: number | number[]
@@ -15,104 +16,107 @@ type ThrusterEffortIndicatorSliderProps = {
     handlerChange: (event: React.ChangeEvent<{}>, value: number | number[]) => void;
 };
 
-const GenericThrusterEffortIndicatorSlider = withStyles({
+const Select: FunctionComponent<ThrusterEffortIndicatorSliderProps> = (props) => {
 
-    root: {
-        "&.MuiSlider-vertical": {
-            width: '80px',
-            padding: '0px 22px',
-            marginTop: '30px',
-        },
-    },
+    const context = useContext(GeneralContext)
+    const GenericThrusterEffortIndicatorSlider = withStyles({
 
-    thumb: {
-        "&.Mui-disabled": {
-            color: 'gba(37, 35, 35, 0.64)',
-            width: '72px',
-            height: '3px',
-            borderRadius: '0px',
-            "& .triangleLeft": {
-                height: 0,
-                width: 0,
-                borderTop: '12px solid transparent',
-                borderBottom: '12px solid transparent',
-                borderLeft: '16px solid currentColor',
-                marginRight: '38px',
-                marginTop: '0px'
-            },
-            "& .triangleRight": {
-                height: 0,
-                width: 0,
-                borderTop: '12px solid transparent',
-                borderBottom: '12px solid transparent',
-                borderRight: '16px solid currentColor',
-                marginLeft: '20px',
-                marginTop: '0px'
+        root: {
+            "&.MuiSlider-vertical": {
+                width: '80px',
+                padding: '0px 22px',
+                marginTop: '30px',
             },
         },
-    },
 
-    vertical: {
-        "& .MuiSlider-thumb": {
+        thumb: {
             "&.Mui-disabled": {
-                marginBottom: '0px',
-                marginLeft: '-6px'
-            }
-        }
-    },
-    mark: {
-        color: 'black',
-        backgroundColor: 'black',
-        opacity: 1.0,
-        width: '60px',
-        height: '3px'
-    },
-    marked: {
-        "&.MuiSlider-vertical": {
-            marginBottom: '30px',
-            marginTop: '30px',
+                color: 'gba(37, 35, 35, 0.64)',
+                width: '72px',
+                height: '3px',
+                borderRadius: '0px',
+                "& .triangleLeft": {
+                    height: 0,
+                    width: 0,
+                    borderTop: '12px solid transparent',
+                    borderBottom: '12px solid transparent',
+                    borderLeft: '16px solid currentColor',
+                    marginRight: '38px',
+                    marginTop: '0px'
+                },
+                "& .triangleRight": {
+                    height: 0,
+                    width: 0,
+                    borderTop: '12px solid transparent',
+                    borderBottom: '12px solid transparent',
+                    borderRight: '16px solid currentColor',
+                    marginLeft: '20px',
+                    marginTop: '0px'
+                },
+            },
         },
-    },
-    markActive: {
-        color: 'black',
-        backgroundColor: 'black',
-        opacity: 1.0,
-        width: '60px',
-    },
-    rail: {
-        color: 'black',
-        opacity: 1.0,
-        marginLeft: '30px'
-    },
-    track: {
-        color: 'black',
-        opacity: 1.0,
-        marginLeft: '30px'
-    },
-    markLabel: {
-        //color: context.isDarkMode ? "white" : 'black',
-        color: "white",
-        marginLeft: '80px',
-        fontWeight: 'bold'
-    },
-})(MUISlider);
 
-const Select: FunctionComponent<ThrusterEffortIndicatorSliderProps> = (props) => (
-    <GenericThrusterEffortIndicatorSlider
-        data-testid="test-ThrusterEffortIndicatorSlider"
-        value={props.value}
-        orientation={props.orientation}
-        min={props.min}
-        max={props.max}
-        disabled={props.disabled}
-        valueLabelDisplay={props.valueLabelDisplay}
-        marks={props.marks}
-        ThumbComponent={props.ThumbComponent}
-        defaultValue={props.defaultValue}
-        onChange={props.handlerChange}
-    >
-    </GenericThrusterEffortIndicatorSlider>
-);
+        vertical: {
+            "& .MuiSlider-thumb": {
+                "&.Mui-disabled": {
+                    marginBottom: '0px',
+                    marginLeft: '-6px'
+                }
+            }
+        },
+        mark: {
+            color: 'black',
+            backgroundColor: 'black',
+            opacity: 1.0,
+            width: '60px',
+            height: '3px'
+        },
+        marked: {
+            "&.MuiSlider-vertical": {
+                marginBottom: '30px',
+                marginTop: '30px',
+            },
+        },
+        markActive: {
+            color: 'black',
+            backgroundColor: 'black',
+            opacity: 1.0,
+            width: '60px',
+        },
+        rail: {
+            color: 'black',
+            opacity: 1.0,
+            marginLeft: '30px'
+        },
+        track: {
+            color: 'black',
+            opacity: 1.0,
+            marginLeft: '30px'
+        },
+        markLabel: {
+            color: context.isDarkMode ? "white" : 'black',
+            marginLeft: '80px',
+            fontWeight: 'bold'
+        },
+    })(MUISlider);
+
+    return (
+        <GenericThrusterEffortIndicatorSlider
+            data-testid="test-ThrusterEffortIndicatorSlider"
+            value={props.value}
+            orientation={props.orientation}
+            min={props.min}
+            max={props.max}
+            disabled={props.disabled}
+            valueLabelDisplay={props.valueLabelDisplay}
+            marks={props.marks}
+            ThumbComponent={props.ThumbComponent}
+            defaultValue={props.defaultValue}
+            onChange={props.handlerChange}
+        >
+        </GenericThrusterEffortIndicatorSlider>
+    )
+};
 
 Select.defaultProps = {
     disabled: false,

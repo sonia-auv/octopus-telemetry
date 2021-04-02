8d35eed96787b679532d8b104330366d5127b318
Laurent Boisvert <laurent.boisvert@ivadolabs.com>
2021-04-01 23:46:49 -0400

    Add zoom on Module thumbnail click

--- src/src/components/modulepicker/Module.tsx
+++ src/src/components/modulepicker/Module.tsx
@@ -1,4 +1,5 @@
 import Check from '../common/check/Check';
+import ImageZoom from 'react-medium-image-zoom';
 
 type ModuleProps = {
   name: string;
@@ -15,15 +16,18 @@ const Module = (props: ModuleProps) => (
     data-testid={`test-drag-${props.name}-${props.id}`}
     key={props.id}
   >
-    <Check
-      label={props.name}
-      value={props.inUse}
-      handler={props.toggleInUse}
-    />
-    <img
-      className="ModulePicker__module-thumbnail"
-      src={props.thumbnailSource}
-      alt={props.thumbnailLabel}
+    <Check label={props.name} value={props.inUse} handler={props.toggleInUse} />
+    <ImageZoom
+      image={{
+        src: props.thumbnailSource,
+        alt: props.thumbnailLabel,
+        className: 'ModulePicker__module-thumbnail',
+      }}
+      zoomImage={{
+        // TODO so should we add a 'big' version to the metadata?
+        src: 'http://placehold.it/600/600',
+        alt: 'Big boi',
+      }}
     />
   </li>
 );

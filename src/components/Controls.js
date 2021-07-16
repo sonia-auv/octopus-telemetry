e5bafe8af4ac3d19bca3194b3b8161b7ba99e1fb
alexemdesgagne <alexandre1998@live.ca>
2021-07-16 13:22:34 -0400

    Tested dry test button and added active thrusters.

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -3,12 +3,13 @@ import Switch from './common/switch/Switch';
 import Button from './common/button/Button';
 import {GeneralContext} from "../context/generalContext";
 import { useROSService} from '../hooks/useROSService'
-import ROSLIB from 'roslib';
-import { FastfoodOutlined } from '@material-ui/icons';
+import { useROSTopicPublisher, MessageFactory } from "../hooks/useROSTopicPublisher";
+
 
 const Controls = (props) => {
 
     const [dryTestActive, setdryTestActive] = useState(false);
+    const [thrusterActive, setThrusterActive] = useState(false);
 
     const callDryTestService = async () => {
 
@@ -23,7 +24,29 @@ const Controls = (props) => {
         
     )
 
-    const dryTestServiceCall = useROSService(dryTestCallback, "/provider_thrusters/dry_test", "std_srvs/Empty")
+    const thrusterActivation = () => {
+        if(!thrusterActive){
+            var toPublish = MessageFactory({
+                slave: 49,
+                bus: 0,
+                data: true
+            })
+            setThrusterActive(true);
+        }   
+        else{
+            var toPublish = MessageFactory({
+                slave: 49,
+                bus: 0,
+                data: false
+            })
+            setThrusterActive(false);
+        }
+
+        providerPowerMsgPublisher(toPublish);
+    }
+
+    const dryTestServiceCall = useROSService(dryTestCallback, "/provider_thruster/dry_test", "std_srvs/Empty")
+    const providerPowerMsgPublisher = useROSTopicPublisher("/provider_power/activate_all_ps", "/sonia_common/ActivateAllPS");
 
     return (
         <GeneralContext.Consumer>
@@ -41,11 +64,14 @@ const Controls = (props) => {
                             value={context.isRelativeUnits}
                             handler={() => context.setIsRelativeUnits(!context.isRelativeUnits)}/>
                     
-                    <Button disabled={context.isDryRunMode } style={{ marginLeft: '25%' , fontSize: '9px' }} handler={callDryTestService} label="Dry Test"/>
+                    <Button disabled={context.isDryRunMode } style={{ marginLeft: '15%' , width: '100px', fontSize: '9px' }} handler={callDryTestService} label="Dry Test"/>
+                    <Button disabled={ context.isDryRunMode} style={{ marginLeft: '15%', marginTop: '10px' , width: '100px', fontSize: '9px' }} 
+                            handler={thrusterActivation} label={ !thrusterActive ? "Activate" : "Deactivate" }/>
                 </div>
             )}
         </GeneralContext.Consumer>
     );
+
 };
 
 export default Controls;

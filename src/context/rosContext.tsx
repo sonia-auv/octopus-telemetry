eee743d8fc7422bb8893dd2f519b01cdb982996a
Guillaume Tremblay <tremblay.9.0.0.0@gmail.com>
2021-07-30 14:59:41 -0400

    Rename env var

--- src/context/rosContext.tsx
+++ src/context/rosContext.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import * as ROSLIB from 'roslib';
 
 export const RosContext = React.createContext(new ROSLIB.Ros({
-    url: process.env.ROS_MASTER_URI ? process.env.ROS_MASTER_URI : 'ws://localhost:9090'
+    url: process.env.ROS_BRIDGE_URI ? process.env.ROS_BRIDGE_URI : 'ws://localhost:9090'
 }))

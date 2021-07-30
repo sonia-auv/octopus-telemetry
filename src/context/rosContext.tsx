35030a0e13e7a074735f411470069728fd2a755e
Guillaume Tremblay <tremblay.9.0.0.0@gmail.com>
2021-07-30 15:37:42 -0400

    Undo changes

--- src/context/rosContext.tsx
+++ src/context/rosContext.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import * as ROSLIB from 'roslib';
 
 export const RosContext = React.createContext(new ROSLIB.Ros({
-    url: process.env.ROS_BRIDGE_URI ? process.env.ROS_BRIDGE_URI : 'ws://localhost:9090'
+    url: 'ws://localhost:9090'
 }))

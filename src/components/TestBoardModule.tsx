4a976cdb4dc09d7643a82d14c9eee48d45885e65
Kam0ss <camille.sauvain.1@etsmtl.net>
2021-09-15 16:37:51 -0400

    changed type

--- src/components/TestBoardModule.tsx
+++ src/components/TestBoardModule.tsx
@@ -15,7 +15,7 @@ const TestBoardModule = () => {
     const [rate, setRate] = React.useState("")
     const [isSingleSend, setIsSingleSend] = React.useState(true)
     let intervalVar: any
-    var splitData: [number,string]
+    let splitData: number[]
     const testBoardPublisher = useROSTopicPublisher<any>("/interface_rs485/dataRx", "sonia_common/SendRS485Msg")
 
     const handleStart = () => {

7b2481215720e85c9aa4ca1df6f12d09c2db8a6f
Kam0ss <camille.sauvain.1@etsmtl.net>
2021-09-15 18:47:55 -0400

    added reset

--- src/components/TestBoardModule.tsx
+++ src/components/TestBoardModule.tsx
@@ -15,10 +15,12 @@ const TestBoardModule = () => {
     const [rate, setRate] = React.useState("")
     const [isSingleSend, setIsSingleSend] = React.useState(true)
     let intervalVar: any
-    let splitData: number[] = []
+    let splitData: number[]
     const testBoardPublisher = useROSTopicPublisher<any>("/interface_rs485/dataRx", "sonia_common/SendRS485Msg")
 
     const handleStart = () => {
+        
+        splitData = []
 
         let table = data.split(",")
         for (var i of table) {

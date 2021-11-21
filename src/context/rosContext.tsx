14d73346d28f0a3ef123100ce23cfa195a10fe5c
alexemdesgagne <alexandre1998@live.ca>
2021-11-21 09:28:52 -0500

    Added env variable.

--- src/context/rosContext.tsx
+++ src/context/rosContext.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import * as ROSLIB from 'roslib';
 
 export const RosContext = React.createContext(new ROSLIB.Ros({
-    url: 'ws://process.env.ROS_IP:9090'
+    url: 'ws://' + process.env.ROS_IP + ':9090'
 }))

0a0ceeefc8e3ce7a484fbd67fa5c3693af354d6b
Kam0ss <camille.sauvain.1@etsmtl.net>
2021-11-20 23:35:44 -0500

    changed back for the sub

--- src/context/rosContext.tsx
+++ src/context/rosContext.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import * as ROSLIB from 'roslib';
 
 export const RosContext = React.createContext(new ROSLIB.Ros({
-    url: `ws://${process.env.ROS_IP}:9090`
+    // TODO try this (after competition) url: `ws://${process.env.ROS_IP}:9090`
+    url: `ws://192.168.0.31:9090`
 }))

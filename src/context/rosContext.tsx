990ab39cf6b981bb6b57e63abbc0f84d8602b5be
Kam0ss <camille.sauvain.1@etsmtl.net>
2021-11-20 23:26:37 -0500

    try on sub

--- src/context/rosContext.tsx
+++ src/context/rosContext.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import * as ROSLIB from 'roslib';
 
 export const RosContext = React.createContext(new ROSLIB.Ros({
-    url: `ws://192.168.0.107:9090`
+    url: `ws://${process.env.ROS_IP}:9090`
 }))
